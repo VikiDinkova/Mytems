251a94de4fc1857631959b6d0e4ec9452759f22e
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2016-10-01 15:26:02 +0300

    Update views and urls

--- Mytems/home/views.py
+++ Mytems/home/views.py
@@ -2,4 +2,20 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, 'category.html', {})
+
+
+def error(request):
+    return render(request, '404.html', {})
+
+
+def detail(request):
+    return render(request, 'detail.html', {})
+
+
+def faq(request):
+    return render(request, 'faq.html', {})
+
+
+def sign_in(request):
+    return render(request, 'sign-in.html', {})

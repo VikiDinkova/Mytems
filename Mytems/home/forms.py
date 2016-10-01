307b534fb276adb91f8f3e031fea1802a4ee3b0f
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2016-10-01 17:29:20 +0300

    Fix model forms

--- Mytems/home/forms.py
+++ Mytems/home/forms.py
@@ -11,28 +11,34 @@ class RegisterForm(forms.Form):
 class DrugForm(forms.ModelForm):
     class Meta:
         model = Drug
+        fields = '__all__'
 
 
 class ClothesForm(forms.ModelForm):
     class Meta:
         model = Clothes
+        fields = '__all__'
 
 
 class TechAccessoryForm(forms.ModelForm):
     class Meta:
         model = TechAccessory
+        fields = '__all__'
 
 
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
+        fields = '__all__'
 
 
 class ClothesUseForm(forms.ModelForm):
     class Meta:
         model = ClothesUse
+        fields = '__all__'
 
 
 class TechUseForm(forms.ModelForm):
     class Meta:
         model = TechUse
+        fields = '__all__'
